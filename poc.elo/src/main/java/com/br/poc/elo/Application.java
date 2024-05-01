b41ebe0dee06450b4ce660af04880613c8996d60
Valter Apolinario <valter_123@hotmail.com>
2024-04-30 21:49:56 -0300

    build: realizado novo build para ajustar erros de estrutura

--- poc.elo/src/main/java/com/br/poc/elo/Application.java
+++ poc.elo/src/main/java/com/br/poc/elo/Application.java
@@ -1,9 +1,15 @@
 package com.br.poc.elo;
 
+import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
+@EnableKafka
 public class Application {
 
 	public static void main(String[] args) {
