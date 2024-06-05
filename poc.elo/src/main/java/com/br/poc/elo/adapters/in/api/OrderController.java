fa509f9cc55fc4b2882d914c8ffc281f402f4b6c
valter.junior <valter_123@hotmail.com>
2024-06-05 17:35:20 -0300

    adicionado configs para swagger

--- poc.elo/src/main/java/com/br/poc/elo/adapters/in/api/OrderController.java
+++ poc.elo/src/main/java/com/br/poc/elo/adapters/in/api/OrderController.java
@@ -2,10 +2,18 @@ package com.br.poc.elo.adapters.in.api;
 
 import com.br.poc.elo.application.ports.incoming.ConsultOrderUseCase;
 import com.br.poc.elo.application.ports.incoming.CreateOrderUseCase;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("api/v1/orders")
@@ -20,9 +28,19 @@ public class OrderController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public ResponseEntity<String> createOrder(@RequestBody OrderRequest request) {
+    @Operation(summary = "Create new order", description = "Create order", tags = {"Order"})
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Order created", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = String.class))
+            })
+    })
+    public ResponseEntity<String> createOrder(
+            @Parameter(description = " new order") @RequestBody OrderRequest request,
+            @Parameter(description = "idempotency key", example = "ffc2a683-8f90-4571-98cb-de7e1bc21e2a",
+                    required = true, schema = @Schema(implementation = UUID.class)) @RequestHeader(value = "idempotency-key") UUID idempotencyKey){
         return ResponseEntity.ok(createUseCase.createOrder(request));
     }
+
 
     @GetMapping("/{id}")
     public ResponseEntity<OrderResponse> consultOrder(@PathVariable String id) {
